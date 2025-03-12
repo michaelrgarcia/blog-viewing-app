61f02d41df4471d0ff8a77098a5af7b23ae52032
Michael Garcia <134650278+michaelrgarcia@users.noreply.github.com>
2025-03-12 03:14:17 -0700

    Update DeleteComment.tsx

--- src/components/App/Comment/popups/DeleteComment.tsx
+++ src/components/App/Comment/popups/DeleteComment.tsx
@@ -1,4 +1,4 @@
-import { Dispatch, SetStateAction, useState } from "react";
+import { Dispatch, SetStateAction } from "react";
 
 import { useAuth } from "../../../../context/AuthProvider";
 import { usePostContext } from "../../../../context/PostContextProvider";
