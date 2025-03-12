78dc165d840d5aead9a0000ab9d18dffac6a6b33
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2025-03-12 00:04:19 -0700

    Dummy commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import App from "./components/App/App.tsx";
+import routes from "./routes.tsx";
 
 import "./index.css";
 
+const router = createBrowserRouter(routes);
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
